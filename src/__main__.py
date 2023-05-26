c4aa783a77812d70520b0e7171d41a20d5380457
よね/Yone <admin@yoneyo.com>
2023-05-26 18:15:12 +0900

    Update

--- src/__main__.py
+++ src/__main__.py
@@ -10,6 +10,8 @@ Licensed under the Apache License 2.0
 import os
 import discord
 from data import config
+import datetime
+import sqlite3
 
 
 # -------------------- Init -------------------- #
@@ -27,6 +29,12 @@ intents = discord.Intents.all()
 intents.message_content = True
 client = discord.Client(intents=intents)
 cmdTree = discord.app_commands.CommandTree(client=client)
+
+db_con = sqlite3.connect("bot-savedata.db")
+db_cur = db_con.cursor()
+
+db_cur.execute("CREATE TABLE IF NOT EXISTS globalBannedList(uid, datetime)")
+db_con.commit()
 
 
 # -------------------- Main -------------------- #
@@ -69,7 +77,7 @@ async def on_message(message):
                     color=0xff4040
                 )
                 .set_footer(
-                    text="警告種別コード: 0x01001"
+                    text="警告種別コード: 0x0101"
                 ),
                 delete_after=15
             )
@@ -137,13 +145,58 @@ async def on_member_join(member):
         channel = client.get_channel(config.joinedChannels[member.guild.id])
         await channel.send(embed=embed)
 
+    # global ban
+    try:
+        db_cur.execute(f"SELECT uid FROM globalBannedList WHERE uid=?", (str(member.id),))
+        data = db_cur.fetchall()
+
+    except Exception as e:
+        print(f"[ERROR] {e}")
+        return
+
+    if data:
+        try:
+            await member.ban(reason="グローバルBANリストに登録されているため")
+
+            try:
+                embed=discord.Embed(
+                    title="Global Ban",
+                    description=f"{member.name} はグローバルBANリストに登録されているため、BANされました。",
+                    color= 0x40ff40,
+                )
+                embed.set_thumbnail(url=member.avatar)
+                channel = client.get_channel(config.joinedChannels[member.guild.id])
+                await channel.send(embed=embed)
+
+            except:
+                pass
+
+        except Exception as e:
+            try:
+                embed=discord.Embed(
+                    title=f"エラーが発生しました",
+                    color= 0xff4040,
+                    description=f"{member.name} はグローバルBANリストに登録されているため、BANを試みましたが失敗しました。```{e}```"
+                )
+                embed.set_thumbnail(url=member.avatar)
+                embed.set_footer(
+                    text=f"エラーコード: 0x0301"
+                )
+
+                channel = client.get_channel(config.joinedChannels[member.guild.id])
+                await channel.send(embed=embed)
+
+            except:
+                pass
+
+
     # welcome
     if member.bot == False:
         if member.guild.id in config.welcomeChannels:
             channel = client.get_channel(config.welcomeChannels[member.guild.id])
             ruleChannel = client.get_channel(config.ruleChannels[member.guild.id])
 
-            if member.guild.id in {}:  # My server
+            if member.guild.id in {1053378444781703339}:  # My server
                 await channel.send(
                     content=f"{member.mention} さん。\n\n"+
                             f"ようこそ {member.guild.name} へ！\n"+
@@ -245,6 +298,133 @@ async def clear(inter: discord.Interaction, target:int):
 
         return
     
+    else:
+        embed = discord.Embed(
+            title="Error",
+            color= 0xff4040,
+            description="あなたはこのコマンドを実行する権限がありません。"
+        )
+        await inter.response.send_message(embed=embed, ephemeral=True)
+        return
+
+
+# ----- global ban add ----- #
+@cmdTree.command(
+    name="gban-add",
+    description="User add to global banned list."
+)
+async def gban_add(inter: discord.Interaction, target: discord.Member):
+    if inter.user.guild_permissions.administrator:
+        try:
+            db_cur.execute(f"SELECT uid, datetime FROM globalBannedList WHERE uid=?", (str(target.id),))
+            data = db_cur.fetchall()
+
+        except Exception as e:
+            await inter.response.send_message(
+                embed=discord.Embed(
+                    title="エラーが発生しました",
+                    color= 0xff4040,
+                    description=f"データベースの読み込みに失敗しました。```{e}```"
+                )
+                .set_footer(
+                    text=f"エラーコード: 0x0201"
+                ),
+                ephemeral=True
+            )
+            return
+
+        if (not data) or (data is None):
+            dt_Now = datetime.datetime.now()
+            add_datetime = dt_Now.strftime('%Y%m%d%H%M%S')
+
+            insertData = (str(target.id), add_datetime)
+            db_cur.execute("INSERT INTO globalBannedList VALUES(?, ?)", insertData)
+            db_con.commit()
+
+            await inter.response.send_message(
+                embed=discord.Embed(
+                    title="Global Ban",
+                    color= 0x40ff40,
+                    description= f"{target.mention} をGlobal Bannedリストに登録しました。"
+                ),
+                ephemeral=True
+            )
+            return
+        
+        else:
+            await inter.response.send_message(
+                embed=discord.Embed(
+                    title="Global Ban",
+                    color= 0xff4040,
+                    description= "このユーザーはすでにGlobal Bannedリストに登録されています。"
+                ),
+                ephemeral=True
+            )
+            return
+
+    else:
+        embed = discord.Embed(
+            title="Error",
+            color= 0xff4040,
+            description="あなたはこのコマンドを実行する権限がありません。"
+        )
+        await inter.response.send_message(embed=embed, ephemeral=True)
+        return
+
+
+# ----- global ban remove ----- #
+@cmdTree.command(
+    name="gban-rmv",
+    description="User remove from global banned list."
+)
+@discord.app_commands.describe(
+    target="リストから削除するユーザーのID"
+)
+async def gban_rmv(inter: discord.Interaction, target:str):
+    if inter.user.guild_permissions.administrator:
+        try:
+            db_cur.execute(f"SELECT uid FROM globalBannedList WHERE uid=?", (target,))
+            data = db_cur.fetchall()
+
+        except Exception as e:
+            await inter.response.send_message(
+                embed=discord.Embed(
+                    title="エラーが発生しました",
+                    color= 0xff4040,
+                    description=f"データベースの読み込みに失敗しました。```{e}```"
+                )
+                .set_footer(
+                    text=f"エラーコード: 0x0201"
+                ),
+                ephemeral=True
+            )
+            return
+
+        if data:
+            db_cur.execute("DELETE FROM globalBannedList WHERE uid=?", (target,))
+            db_con.commit()
+
+            await inter.response.send_message(
+                embed=discord.Embed(
+                    title="Global Ban",
+                    color= 0x40ff40,
+                    description= f"<@{target}> をGlobal Bannedリストから削除しました。"
+                ),
+                ephemeral=True
+            )
+            return
+        
+        else:
+            await inter.response.send_message(
+                embed=discord.Embed(
+                    title="Global Ban",
+                    color= 0xff4040,
+                    description= "このユーザーIDはGlobal Bannedリストに登録されていません。"
+                ),
+                ephemeral=True
+            )
+            return
+
     else:
         embed = discord.Embed(
             title="Error",
